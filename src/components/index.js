1c9461568d6dc82d97d3acde9022ef55f118dd12
khoa1232123 <38268470+khoa1232123@users.noreply.github.com>
2023-10-25 23:58:57 +0700

    Initial commit

---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import About from "./About";
+import Contact from "./Contact";
+import Experience from "./Experience";
+import Feedbacks from "./Feedbacks";
+import Hero from "./Hero";
+import Loader from "./Loader";
+import Navbar from "./Navbar";
+import Tech from "./Tech";
+import Works from "./Works";
+import {
+  BallCanvas,
+  ComputersCanvas,
+  EarthCanvas,
+  StarsCanvas,
+} from "./canvas";
+
+export {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Loader,
+  Navbar,
+  Tech,
+  Works,
+  BallCanvas,
+  ComputersCanvas,
+  EarthCanvas,
+  StarsCanvas,
+};
